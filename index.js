ff05c1e6cc97bf363a854a10188d7db959cd379e
Rubén Emmanuel Herrera Flores <111837256+Ruben-HF@users.noreply.github.com>
2023-08-25 16:06:13 -0600

    Welcome Dashboard
    Se inserta el dashboard de bienvenida, pendiente poder mostrar los datos de temperatura con OpenWeather https://openweathermap.org/current

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const dateElement = document.getElementById('date');
-const timeElement = document.getElementById('time');
 const darkModeButton = document.getElementById('darkModeButton');
 const footer = document.querySelector('footer');
 
@@ -9,15 +8,12 @@ function updateDateTime() {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'America/Mexico_City' };
     const dateString = now.toLocaleDateString('es-ES', options);
     
-    const timeString = now.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'America/Mexico_City' });
-    
     dateElement.textContent = dateString;
-    timeElement.textContent = timeString;
 }
 
 
 function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode'); 
+    document.body.classList.toggle('dark-mode');
     footer.classList.toggle('dark-mode');
 
     const darkModeButton = document.getElementById('darkModeButton');
@@ -29,11 +25,29 @@ function toggleDarkMode() {
 }
 
 
+const temperatureElement = document.getElementById('temperature');
+const apiKey = '05103b6cbd8de0e32c07d237637d61df';
+
+async function fetchTemperature() {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Mexico%20City&appid=${apiKey}&units=metric`);
+        const data = await response.json();
+        const temperature = data.main.temp;
+        temperatureElement.textContent = `Temperature: ${temperature}°C`;
+    } catch (error) {
+        console.error('Error fetching temperature:', error);
+        temperatureElement.textContent = 'Temperature: N/A';
+    }
+}
+
+// Llamar a la función de obtención de temperatura
+fetchTemperature();
+
 // Actualizar la fecha y hora cada segundo
 setInterval(updateDateTime, 1000);
 
 // Alternar el modo oscuro al hacer clic en el botón
 darkModeButton.addEventListener('click', toggleDarkMode);
 
-// Llamar a la función de actualización de fecha y hora una vez al cargar la página
+// Llamar a la función de actualización de fecha una vez al cargar la página
 updateDateTime();
